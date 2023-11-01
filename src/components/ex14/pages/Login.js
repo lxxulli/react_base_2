338bcaa57cdd36c84dd0893f3bc370e60e5a3690
lxxulli <lyr98803@naver.com>
2023-11-01 17:22:56 +0900

    리액트 15일차 로그인 세부사항 수정

--- src/components/ex14/pages/Login.js
+++ src/components/ex14/pages/Login.js
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
+import { ErrorMessage } from "../components/ErrorMessage";
 
 const Wrap = styled.div`
   height: 100vh;
@@ -31,6 +32,7 @@ const Input = styled.input`
   width: 100%;
   height: 50px;
   border: 1px solid #dbdbdb;
+  border-radius: 10px;
   padding: 0 15px;
   margin-top: 10px;
 `;
@@ -45,14 +47,24 @@ const Button = styled.button`
   font-size: 18px;
   font-weight: 600;
   color: white;
+  opacity: ${(props) => (props.$isActive ? 1 : 0.5)};
+  cursor: ${(props) => (props.$isActive ? "pointer" : "default")};
 `;
+
+// ? = 참이라면
+// : = 그게 아니라면
 
 export const Login = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm();
+    formState: { errors, isValid },
+  } = useForm({
+    mode: "onChange", // 검색에 대해 즉각 반응을 해줌
+  });
+
+  console.log(isValid);
+  // - 유효성 검사 후 boolean값으로 반환함
 
   // console.log(errors.username.message);
   // 오류뜸
@@ -72,23 +84,61 @@ export const Login = () => {
         <Input
           {...register("username", {
             required: "아이디는 필수입니다.",
+            minLength: {
+              value: 2,
+              message: "아이디는 2자리 이상 작성해 주세요",
+            },
           })}
           type="text"
           placeholder="id"
         />
-        <span>{errors?.username?.message}</span>
+        {/* <span>{errors?.username?.message}</span> */}
+        <ErrorMessage message={errors?.username?.message} />
 
         <Input
-          {...register("username", {
+          {...register("password", {
             required: "패스워드는 필수입니다.",
+            minLength: {
+              value: 8,
+              message: "패스워드는 8자리 이상 작성해 주세요",
+            },
+            pattern: {
+              value:
+                /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+              // 비밀번호 정규식
+              message:
+                "최소 8자 작성이며, 하나 이상의 대문자/소문자/숫자/특수문자를 이용하여 적어주세요.",
+            },
           })}
           type="password"
           placeholder="password"
         />
-        <Button>login</Button>
+        {/* <span>{errors?.password?.message}</span> */}
+        <ErrorMessage message={errors?.password?.message} />
+
+        <Button $isActive={isValid}>login</Button>
       </Form>
     </Wrap>
   );
 };
+
+// *useForm();
+// - 폼 관련 패키지
+// ex)
+// const {
+//   register,  // input태그 name 및 등록 역할
+//   handleSubmit,  // form태그 이벤트 등록
+//   formState: { errors },  // form상태를 관리
+//                           // errors : form 유효성 검사 후 에러를 객체로 반환함
+//                           // isValid : form상태가 유효한 지 boolean값으로 반환함
+// } = useForm({
+// mode: "onChange" // form 모드로 유효성 검사를 어떻게 처리할 지 값을 작성할 수 있음
+// });
+
+{
+  /* <input {...register("name명", {
+  required: ""   // 현 input값이 필수값인지 아닌지 boolean 및 문자열로 작성 가능
+})}/> */
+}
 
 // required = 필수
